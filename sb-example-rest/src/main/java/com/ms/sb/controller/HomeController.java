b0432b49e8ae1d787b27938eca1afb23441cb3f7
tkmas43 <tkmas43@VGFCVIEW01431.cp.ad.kohls.com>
2017-08-19 21:39:15 +0530

    Shipwreck

---
+++ sb-example-rest/src/main/java/com/ms/sb/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ms.sb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "back to work";
+	}
+}
---
+++ sb-example-rest/src/main/java/com/ms/sb/repository/ShipwreckRepository.java
@@ -0,0 +1,9 @@
+package com.ms.sb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ms.sb.model.Shipwreck;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long>{
+
+}
